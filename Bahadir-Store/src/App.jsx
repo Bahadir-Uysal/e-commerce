8c1a2a035757dd2828f72348243b6f417c0fd6ab
Bahadır UYSAL <uysalbahadir1529@gmail.com>
2024-10-14 13:11:19 +0300

    toastify

--- Bahadir-Store/src/App.jsx
+++ Bahadir-Store/src/App.jsx
@@ -6,6 +6,7 @@ function App() {
     <>
       <h1 className="text-xl text-blue-600">bahadır uysal</h1>
       <div className="bg-blue-500 text-white p-4">Tailwind çalışıyor!</div>
+      <i class="fa-solid fa-user"></i>
     </>
   )
 }
