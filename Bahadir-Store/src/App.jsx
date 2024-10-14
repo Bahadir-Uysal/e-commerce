f1c147cad4bb5954b0623816313e0ad77917743c
Bahadır UYSAL <uysalbahadir1529@gmail.com>
2024-10-14 12:43:32 +0300

    setup

--- Bahadir-Store/src/App.jsx
+++ Bahadir-Store/src/App.jsx
@@ -4,7 +4,8 @@ function App() {
 
   return (
     <>
-      
+      <h1 className="text-xl text-blue-600">bahadır uysal</h1>
+      <div className="bg-blue-500 text-white p-4">Tailwind çalışıyor!</div>
     </>
   )
 }
