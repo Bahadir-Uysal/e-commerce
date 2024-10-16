a5cfb31daf15165a3d2614165986628e80653f76
Bahadır UYSAL <uysalbahadir1529@gmail.com>
2024-10-16 11:52:25 +0300

    T02: toggling the menu  #2

--- Bahadir-Store/src/layout/Header.jsx
+++ Bahadir-Store/src/layout/Header.jsx
@@ -1,24 +1,35 @@
+import { useState } from "react";
+
 function Header() {
+    const [hamburgerOpen,setHamburgerOpen] = useState(false);
+
+    function toggleHamburger(){
+        setHamburgerOpen(!hamburgerOpen)
+    }
   return (
     <>
-      <div className="">
-        <div className="flex justify-around">
-          <h1>Bahadır Store</h1>
-          <div className="flex gap-4">
+      <nav className= "bg-white p-4 shadow-md">
+        <div className="flex justify-around items-center">
+          <h1 className="text-xl font-bold">Bahadır Store</h1>
+          <div className="flex gap-7">
             <i className="fa fa-user"></i>
             <i className="fa fa-search"></i>
             <i className="fa fa-shopping-cart"></i>
+            <button onClick={toggleHamburger} >
             <i className="fa fa-bars"></i>
+            </button>
           </div>
         </div>
 
-        <div className="flex flex-col items-center">
-          <a href="">Home</a>
-          <a href="">Product</a>
-          <a href="">Pricing</a>
-          <a href="">Contact</a>
-        </div>
-      </div>
+        {hamburgerOpen && (
+            <div className="flex flex-col items-between">
+            <a href="">Home</a>
+            <a href="">Product</a>
+            <a href="">Pricing</a>
+            <a href="">Contact</a>
+          </div>
+        )}
+      </nav>
     </>
   );
 }
