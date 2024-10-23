b9938a4bba4b14f994a69406853c3c6c7f97e72e
Bahadır UYSAL <uysalbahadir1529@gmail.com>
2024-10-23 16:42:42 +0300

    T03: Shop PagePRoductContent Done  #4

--- Bahadir-Store/src/layout/ShopPageContent.jsx
+++ Bahadir-Store/src/layout/ShopPageContent.jsx
@@ -1,11 +1,13 @@
 import FilterComp from "@/components/FilterComp";
 import ShopCloths from "@/components/ShopCloths";
+import ShopPageProductContent from "@/components/ShopPageProductContent";
 
 function ShopPageContent() {
   return (
     <>
       <ShopCloths></ShopCloths>
       <FilterComp></FilterComp>
+      <ShopPageProductContent></ShopPageProductContent>
     </>
   );
 }
