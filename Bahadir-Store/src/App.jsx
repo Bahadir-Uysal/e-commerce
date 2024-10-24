088fd8977d6f54c3db6b29e48132a6a9cffa4bad
Bahadır UYSAL <uysalbahadir1529@gmail.com>
2024-10-24 14:39:33 +0300

    T05: Contact Page Done #9

--- Bahadir-Store/src/App.jsx
+++ Bahadir-Store/src/App.jsx
@@ -2,20 +2,24 @@ import HomePage from "./pages/HomePage";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import ShopPage from "./pages/ShopPage";
 import ProductPage from "./pages/ProductPage";
+import ContactPage from "./pages/ContactPage";
 
 function App() {
   return (
     <>
       <Router>
         <Switch>
-        <Route path="/product/:id" component={ProductPage}>
-            <ProductPage></ProductPage>
+          <Route path="/" exact component={HomePage}>
+            <HomePage></HomePage>
           </Route>
           <Route path="/shop" component={ShopPage}>
             <ShopPage></ShopPage>
           </Route>
-          <Route path="/" component={HomePage}>
-            <HomePage></HomePage>
+          <Route path="/product/:id" component={ProductPage}>
+            <ProductPage></ProductPage>
+          </Route>
+          <Route>
+            <ContactPage path="/contact" component={ContactPage}></ContactPage>
           </Route>
         </Switch>
       </Router>
