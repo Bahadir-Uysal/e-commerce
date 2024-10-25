fa4df8c006cf2827e803018a99b20943767d9acc
Bahadır UYSAL <uysalbahadir1529@gmail.com>
2024-10-25 14:22:58 +0300

    T07: About Us Page VideoContainer Done #13

--- Bahadir-Store/src/layout/AboutUsPageContent.jsx
+++ Bahadir-Store/src/layout/AboutUsPageContent.jsx
@@ -1,12 +1,14 @@
 import AboutUsBanner from "@/components/AboutUsBanner"
 import AboutUsSpace from "@/components/AboutUsSpace"
 import Status from "@/components/Status"
+import VideoContainer from "@/components/VideoContainer"
 
 function AboutUsPageContent(){
     return(<>
     <AboutUsBanner></AboutUsBanner>
     <AboutUsSpace></AboutUsSpace>
     <Status></Status>
+    <VideoContainer></VideoContainer>
     </>)
 }
 
