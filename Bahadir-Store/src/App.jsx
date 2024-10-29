812f8f2f1f26ba3909164f236c514f6efb393d99
Bahadır UYSAL <uysalbahadir1529@gmail.com>
2024-10-29 14:05:23 +0300

    T08: Sign Up User Form Page Fixes #15

--- Bahadir-Store/src/App.jsx
+++ Bahadir-Store/src/App.jsx
@@ -5,21 +5,35 @@ import ProductPage from "./pages/ProductPage";
 import ContactPage from "./pages/ContactPage";
 import TeamPage from "./pages/TeamPage";
 import AboutUsPage from "./pages/AboutUsPage";
+import SignUpPage from "./pages/SignUpPage";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
-    <>
-       <Router>
-      <Switch>
-        <Route path="/" exact component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
-        <Route path="/product/:id" component={ProductPage} />
-        <Route path="/contact" component={ContactPage} />
-        <Route path="/team" component={TeamPage} />
-        <Route path="/AboutUs" component={AboutUsPage} />
-      </Switch>
+    <Router>
+      <>
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/product/:id" component={ProductPage} />
+          <Route path="/contact" component={ContactPage} />
+          <Route path="/team" component={TeamPage} />
+          <Route path="/AboutUs" component={AboutUsPage} />
+          <Route path="/signup" component={SignUpPage} />
+        </Switch>
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+      </>
     </Router>
-    </>
   );
 }
 
---
+++ Bahadir-Store/src/pages/SignUpPage.jsx
@@ -0,0 +1,14 @@
+import Header from "@/layout/Header";
+import Footer from "./Footer";
+import SignUpPageContent from "@/layout/SingUpPageContent";
+
+function SignUpPage() {
+  return (
+    <>
+      <Header></Header>
+      <SignUpPageContent></SignUpPageContent>
+      <Footer></Footer>
+    </>
+  );
+}
+export default SignUpPage;
